85694ba770ad0d2eb234052e1cb30aa54c382737
Kavya Naveena <karumurikavya@gmail.com>
2023-03-06 22:43:10 +0530

    roboshop automation

--- roboshop-setup/ex1/servers.tf
+++ roboshop-setup/ex1/servers.tf
@@ -7,6 +7,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z0460963X4DZ0AWOHBFX"
+  name    = "frontend-dev.learndevopsb71club.site"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "cart" {
   ami                       = "ami-0a017d8ceb274537d"
   instance_type             =  "t3.micro"
@@ -14,6 +22,14 @@ resource "aws_instance" "cart" {
   tags = {
     Name = "cart"
   }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z0460963X4DZ0AWOHBFX"
+  name    = "cart-dev.learndevopsb71club.site"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.cart.private_ip]
 }
 
 resource "aws_instance" "catalogue" {
@@ -25,6 +41,14 @@ resource "aws_instance" "catalogue" {
   }
 }
 
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z0460963X4DZ0AWOHBFX"
+  name    = "catalogue-dev.learndevopsb71club.site"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.catalogue.private_ip]
+}
+
 resource "aws_instance" "user" {
   ami                       = "ami-0a017d8ceb274537d"
   instance_type             =  "t3.micro"
@@ -32,6 +56,13 @@ resource "aws_instance" "user" {
   tags = {
     Name = "user"
   }
+}
+resource "aws_route53_record" "user" {
+  zone_id = "Z0460963X4DZ0AWOHBFX"
+  name    = "user-dev.learndevopsb71club.site"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.user.private_ip]
 }
 
 resource "aws_instance" "shipping" {
@@ -42,6 +73,13 @@ resource "aws_instance" "shipping" {
     Name = "shipping"
   }
 }
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z0460963X4DZ0AWOHBFX"
+  name    = "shipping-dev.learndevopsb71club.site"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.shipping.private_ip]
+}
 
 resource "aws_instance" "payment" {
   ami                       = "ami-0a017d8ceb274537d"
@@ -50,6 +88,13 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "test"
   }
+}
+resource "aws_route53_record" "payment" {
+  zone_id = "Z0460963X4DZ0AWOHBFX"
+  name    = "payment-dev.learndevopsb71club.site"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.payment.private_ip]
 }
 
 resource "aws_instance" "mongodb" {
@@ -60,6 +105,13 @@ resource "aws_instance" "mongodb" {
     Name = "mongodb"
   }
 }
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z0460963X4DZ0AWOHBFX"
+  name    = "mongodb-dev.learndevopsb71club.site"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mongodb.private_ip]
+}
 
 resource "aws_instance" "mysql" {
   ami                       = "ami-0a017d8ceb274537d"
@@ -68,6 +120,13 @@ resource "aws_instance" "mysql" {
   tags = {
     Name = "test"
   }
+}
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z0460963X4DZ0AWOHBFX"
+  name    = "mysql-dev.learndevopsb71club.site"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mysql.private_ip]
 }
 
 resource "aws_instance" "rabbitmq" {
@@ -78,6 +137,13 @@ resource "aws_instance" "rabbitmq" {
     Name = "rabbitmq"
   }
 }
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z0460963X4DZ0AWOHBFX"
+  name    = "rabbitmq-dev.learndevopsb71club.site"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.rabbitmq.private_ip]
+}
 
 resource "aws_instance" "Redis" {
   ami                       = "ami-0a017d8ceb274537d"
@@ -86,4 +152,11 @@ resource "aws_instance" "Redis" {
   tags = {
     Name = "Redis"
   }
+}
+resource "aws_route53_record" "Redis" {
+  zone_id = "Z0460963X4DZ0AWOHBFX"
+  name    = "redis-dev.learndevopsb71club.site"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.Redis.private_ip]
 }
