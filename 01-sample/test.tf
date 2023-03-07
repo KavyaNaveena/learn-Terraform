26847ee8cd57a88906b0cacd9c317e62adc03de9
Kavya Naveena <karumurikavya@gmail.com>
2023-03-07 14:44:37 +0530

    update

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -6,3 +6,10 @@ resource "aws_instance" "ec2" {
     Name = "test"
   }
 }
+
+resource "aws_instance" "frontend" {
+  count                     =  5
+  ami                       = "ami-0a017d8ceb274537d"
+  instance_type             =  "t3.micro"
+  vpc_security_group_ids    =  ["sg-044088839c84c7c16"]
+}
