c6ecd5ab35cdf624b247c5c58691a6a104eeae28
Kavya Naveena <karumurikavya@gmail.com>
2023-03-08 16:09:26 +0530

    update

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -4,3 +4,11 @@ resource "aws_instance" "test" {
 }
 
 variable "instances" {}
+
+variable "create_instance" {}
+
+resource "aws_instance" "test1" {
+  count = var.create_instance == "true" ? 1 : 0
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = var.instances == "" ? "t3.micro" : var.instances
+}
