0b741f56298550653d383ae0b8624fcd3cb112d8
Kavya Naveena <karumurikavya@gmail.com>
2023-03-08 14:34:36 +0530

    update

--- roboshop-setup/ex3/main.tf
+++ roboshop-setup/ex3/main.tf
@@ -26,3 +26,51 @@ variable "instances" {
     }
   }
 }
+
+output "ec2" {
+  value = var.instances["catalogue"].public_ip
+}
+## immature code
+variable "names" {
+  default = ["catalogue","user"]
+}
+
+variable "types" {
+  default = ["t3.micro","t3.small"]
+}
+
+resource "aws_instance" "instances" {
+ count                     = length(var.names)
+ ami                       =  data.aws_ami.ami.image_id
+ instance_type             =  var.types[count.index]
+ vpc_security_group_ids    =  ["sg-044088839c84c7c16"]
+ tags = {
+   Name = var.names[count.index]
+ }
+}
+
+##Little mature code
+/*
+resource "aws_instance" "instances" {
+  for_each                  =  var.demo
+  ami                       =  data.aws_ami.ami.image_id
+  instance_type             =  var.demo[count.index]["type"]
+  vpc_security_group_ids    =  ["sg-044088839c84c7c16"]
+  tags = {
+   Name = var.demo[count.index]["name"]
+ }
+}
+
+variable "demo" {
+   default = [
+   {
+      name = "catalogue"
+      type = "t3.micro"
+    },
+    {
+      name = "user"
+      type = "t3.small"
+    }
+  ]
+}
+*/
